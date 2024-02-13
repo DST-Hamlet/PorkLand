409958355a90704c12e887757562225cf8cbc80b
jerry <2234834467@qq.com>
2024-02-13 22:17:15 +0800

    add pangolden setting

--- postinit/map/forest_map.lua
+++ postinit/map/forest_map.lua
@@ -15,6 +15,7 @@ local MULTIPLY = forest_map.MULTIPLY
 local TRANSLATE_TO_PREFABS = forest_map.TRANSLATE_TO_PREFABS
 local TRANSLATE_AND_OVERRIDE = forest_map.TRANSLATE_AND_OVERRIDE
 
+TRANSLATE_TO_PREFABS["pangolden"] = { "pangolden" }
 TRANSLATE_TO_PREFABS["asparagus"] = { "asparagus_planted" }
 TRANSLATE_TO_PREFABS["peagawk_spawner"] = { "peagawk_spawner" }
 TRANSLATE_TO_PREFABS["grass_tall_patch"] = { "grass_tall_patch" }
