5286df25eae1e40fcc9512c8fe3a8daacaf22cb3
Ardent <47233045+Noctice@users.noreply.github.com>
2024-05-28 02:43:19 +0800

    fix ripple color bug

--- postinit/components/oceancolor.lua
+++ postinit/components/oceancolor.lua
@@ -16,43 +16,6 @@ local _Initialize = OceanColor.Initialize
 function OceanColor:Initialize(has_ocean, ...)
     if TheWorld:HasTag("porkland") then
         TheWorld.Map:SetClearColor(0, 0, 0, 1)
-
-        --修改原版数值，不知道是否应该放这里
-        --用于去除小地图陆地边缘的海洋渐变
-        TUNING.OCEAN_MINIMAP_SHADER.EDGE_COLOR0 = { 0, 0, 0 }
-        TUNING.OCEAN_MINIMAP_SHADER.EDGE_PARAMS0 =
-        {
-            THRESHOLD = 0,
-            HALF_THRESHOLD_RANGE = 0,
-        }
-
-        TUNING.OCEAN_MINIMAP_SHADER.EDGE_COLOR1 = { 0, 0, 0 }
-        TUNING.OCEAN_MINIMAP_SHADER.EDGE_PARAMS1 =
-        {
-            THRESHOLD = 0,
-            HALF_THRESHOLD_RANGE = 0,
-        }
-
-        TUNING.OCEAN_MINIMAP_SHADER.EDGE_SHADOW_COLOR = { 0, 0, 0 }
-        TUNING.OCEAN_MINIMAP_SHADER.EDGE_SHADOW_PARAMS =
-        {
-            THRESHOLD = 0,
-            HALF_THRESHOLD_RANGE = 0,
-            UV_OFFSET_X = 0,
-            UV_OFFSET_Y = 0,
-        }
-
-        TUNING.OCEAN_MINIMAP_SHADER.EDGE_FADE_PARAMS =
-        {
-            THRESHOLD = 0,
-            HALF_THRESHOLD_RANGE = 0,
-            MASK_INSET = 0,
-        }
-
-        TUNING.OCEAN_MINIMAP_SHADER.EDGE_NOISE_PARAMS =
-        {
-            UV_SCALE = 0,
-        }
     else
         _Initialize(self, has_ocean, ...)
     end
