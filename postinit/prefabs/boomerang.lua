98e301e30e0c5ce7524411b232a80ef2eca095ce
Ardent <47233045+Noctice@users.noreply.github.com>
2024-05-30 03:45:20 +0800

    fix bug of boatcontainer RUMMAGE.extra_arrive_dist (#69)
    * add boons
    
    * bug fix
    
    * bug fix
    
    * Update level.lua
    
    * fix bug of boatcontainer RUMMAGE.extra_arrive_dist
    
    * fix bug of boomerang in water
    
    * Update boomerang.lua
    
    * Update boomerang.lua

---
+++ postinit/prefabs/boomerang.lua
@@ -0,0 +1,18 @@
+local AddPrefabPostInit = AddPrefabPostInit
+GLOBAL.setfenv(1, GLOBAL)
+
+local function OnFinished(inst)
+    inst.AnimState:PlayAnimation("used")
+    inst:DoTaskInTime(inst.AnimState:GetCurrentAnimationLength(),inst.Remove)
+end
+
+local function OnDropped(inst)
+    inst.AnimState:PlayAnimation("idle")
+    inst.components.inventoryitem.pushlandedevents = true
+end
+
+AddPrefabPostInit("boomerang", function(inst)
+    inst.components.finiteuses:SetOnFinished(OnFinished)
+
+    inst.components.inventoryitem:SetOnDroppedFn(OnDropped)
+end)
