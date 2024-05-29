5777e6941febce2c397129cf369da896b5d40765
Ardent <47233045+Noctice@users.noreply.github.com>
2024-05-30 03:34:56 +0800

    Update boomerang.lua

--- postinit/prefabs/boomerang.lua
+++ postinit/prefabs/boomerang.lua
@@ -1,12 +1,12 @@
 local AddPrefabPostInit = AddPrefabPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
-local function OnFinished_Pl(inst)
+local function OnFinished(inst)
     inst.AnimState:PlayAnimation("used")
     inst:DoTaskInTime(inst.AnimState:GetCurrentAnimationLength(),inst.Remove)
 end
 
-local function OnDropped_Pl(inst)
+local function OnDropped(inst)
     inst.AnimState:PlayAnimation("idle")
     inst.components.inventoryitem.pushlandedevents = true
 end
