eaf6217ee5b338a4fd9dc8a7b41704bdeb7d843a
Ardent <47233045+Noctice@users.noreply.github.com>
2024-07-12 08:46:46 +0800

    Update vampirebat.lua
    使得蝙蝠阴影像大鹏阴影一样由小变大
    优化了蝙蝠阴影的视觉效果和代码

--- scripts/prefabs/vampirebat.lua
+++ scripts/prefabs/vampirebat.lua
@@ -236,7 +236,11 @@ local function DoDive(inst)
             bat.sg:GoToState("glide")
             bat:AddTag("batfrenzy")
         end
-        inst:Remove()
+        inst.task = nil
+        inst.taskinfo = nil
+        inst.components.colourtweener:StartTween({1,1,1,0}, 0.3)
+        inst.components.circler:Stop()
+        inst:DoTaskInTime(1,inst.Remove)
         return
     end
 
@@ -255,64 +259,25 @@ local function DoDive(inst)
                 bat.components.combat:SuggestTarget(player)
             end)
         end
-        inst:Remove()
+        inst.task = nil
+        inst.taskinfo = nil
+        inst.components.colourtweener:StartTween({1,1,1,0}, 0.3)
+        inst.components.circler:Stop()
+        inst:DoTaskInTime(1,inst.Remove)
     else
         inst.task, inst.taskinfo = inst:ResumeTask(5 + math.random() * 2, DoDive)
     end
 end
 
-local MAX_FADE_FRAME = math.floor(3 / FRAMES + 0.5)
 
-local function OnUpdateFade(inst, dframes)
-    local done
-    if inst._isfadein:value() then
-        local frame = inst._fadeframe:value() + dframes
-        done = frame >= MAX_FADE_FRAME
-        inst._fadeframe:set_local(done and MAX_FADE_FRAME or frame)
-    else
-        local frame = inst._fadeframe:value() - dframes
-        done = frame <= 0
-        inst._fadeframe:set_local(done and 0 or frame)
-    end
-
-    local k = inst._fadeframe:value() / MAX_FADE_FRAME
-    inst.AnimState:OverrideMultColour(1, 1, 1, k)
-
-    if done then
-        inst._fadetask:Cancel()
-        inst._fadetask = nil
-        if inst._killed then
-            --don't need to check ismastersim, _killed will never be set on clients
-            inst:Remove()
-            return
-        end
-    end
-
-    if TheWorld.ismastersim then
-        if inst._fadeframe:value() > 0 then
-            inst:Show()
+local function CircleOnIsNight(inst)
+    if inst.taskinfo ~= nil then
+        if TheWorld.state.isnight then
+            inst.components.colourtweener:StartTween({1,1,1,0}, 3)
         else
-            inst:Hide()
+            inst.components.colourtweener:StartTween({1,1,1,1}, 3)
         end
     end
-end
-
-local function OnFadeDirty(inst)
-    if inst._fadetask == nil then
-        inst._fadetask = inst:DoPeriodicTask(FRAMES, OnUpdateFade, nil, 1)
-    end
-    OnUpdateFade(inst, 0)
-end
-
-local function CircleOnIsNight(inst, isnight)
-    inst._isfadein:set(not isnight)
-    inst._fadeframe:set(inst._fadeframe:value())
-    OnFadeDirty(inst)
-end
-
-local function CircleOnInit(inst)
-    inst:WatchWorldState("isnight", CircleOnIsNight)
-    CircleOnIsNight(inst, TheWorld.state.isnight)
 end
 
 local function OnSaveShadow(inst, data)
@@ -325,6 +290,7 @@ end
 
 local function OnLoadShadow(inst, data)
     if not data then
+        inst:Remove()
         return
     end
 
@@ -356,23 +322,24 @@ local function circlingbatfn()
     inst.AnimState:SetOrientation(ANIM_ORIENTATION.OnGround)
     inst.AnimState:SetLayer(LAYER_BACKGROUND)
     inst.AnimState:SetSortOrder(3)
-    inst.AnimState:OverrideMultColour(1, 1, 1, 0)
+    inst.AnimState:SetMultColour(1, 1, 1, 0)
 
     inst:AddTag("FX")
-
-    inst._fadeframe = net_byte(inst.GUID, "circlingbuzzard._fadeframe", "fadedirty")
-    inst._isfadein = net_bool(inst.GUID, "circlingbuzzard._isfadein", "fadedirty")
-    inst._fadetask = nil
 
     inst.entity:SetPristine()
 
     if not TheWorld.ismastersim then
-        inst:ListenForEvent("fadedirty", OnFadeDirty)
-
         return inst
     end
 
     inst:AddComponent("circler")
+    inst.components.circler.minScale = 10
+    inst.components.circler.maxScale = 8
+
+    inst:AddComponent("colourtweener")
+    if not TheWorld.state.isnight then
+        inst.components.colourtweener:StartTween({1,1,1,1}, 3)
+    end
 
     inst:ListenForEvent("wingdown", OnWingDownShadow)
     -- flap sound
@@ -384,7 +351,8 @@ local function circlingbatfn()
         end
     end)
 
-    inst:DoTaskInTime(0, CircleOnInit)
+    inst:WatchWorldState("isnight", CircleOnIsNight)
+    inst:WatchWorldState("isday", CircleOnIsNight)
 
     inst.task, inst.taskinfo = inst:ResumeTask(20 + math.random() * 2, DoDive)
 
