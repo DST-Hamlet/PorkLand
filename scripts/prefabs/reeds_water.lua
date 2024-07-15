98bf435d116170387fa63a0363d031aa152e4fd3
Manutsawee <manutsawee.louis@outlook.com>
2024-07-15 13:27:07 +0800

    add: reeds_water

---
+++ scripts/prefabs/reeds_water.lua
@@ -0,0 +1,32 @@
+local assets = {
+    Asset("ANIM", "anim/grass_inwater.zip"),
+    Asset("ANIM", "anim/reeds_water_build.zip"),
+}
+
+local prefabs = {
+    "cutreeds",
+}
+
+local fn = function()
+    local inst = Prefabs["reeds"].fn()
+
+    inst.MiniMapEntity:SetIcon("reeds_in_water.png")
+
+    MakeObstaclePhysics(inst, .25)
+
+    inst.AnimState:SetBank("grass_inwater")
+    inst.AnimState:SetBuild("reeds_water_build")
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+    -- inst:AddComponent("appeasement")
+    -- inst.components.appeasement.appeasementvalue = TUNING.WRATH_SMALL
+
+    MakePickableBlowInWindGust(inst, TUNING.REEDS_WINDBLOWN_SPEED, TUNING.REEDS_WINDBLOWN_FALL_CHANCE)
+
+    return inst
+end
+
+return Prefab("reeds_water", fn, assets, prefabs)
