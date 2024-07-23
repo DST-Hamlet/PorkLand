1d54878e5f0d2e9d85a48a9f09e54b647dd7ec8b
Tony <legendmastertony@gmail.com>
2024-07-23 23:00:41 +0800

    Move economy and periodicpoopmanager to porkland

--- postinit/prefabs/world.lua
+++ postinit/prefabs/world.lua
@@ -5,11 +5,4 @@ AddPrefabPostInit("world", function(inst)
     if not TheWorld.components.interiorspawner then
         inst:AddComponent("interiorspawner")
     end
-    if not TheWorld.components.economy then
-        inst:AddComponent("economy")
-        inst.components.economy:AddCity(1)
-    end
-    if not TheWorld.components.periodicpoopmanager then
-        inst:AddComponent("periodicpoopmanager")
-    end
 end)
