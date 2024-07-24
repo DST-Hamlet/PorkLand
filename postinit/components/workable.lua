389a63c79438fff5160416269ba5836584051287
Tony <legendmastertony@gmail.com>
2024-07-24 20:03:30 +0800

    Fix TheWorld typo

--- postinit/components/workable.lua
+++ postinit/components/workable.lua
@@ -6,12 +6,12 @@ local destroy = Workable.Destroy
 function Workable:Destroy(destroyer, ...)
     local ret = { destroy(self, destroyer, ...) }
 
-    if  ThWorld.components.cityalarms
+    if TheWorld.components.cityalarms
         and self.inst.components.citypossession
         and self.inst.components.citypossession.enabled
         and self.inst.components.citypossession.cityID then
 
-        ThWorld.components.cityalarms:ChangeStatus(self.inst.components.citypossession.cityID, true, destroyer)
+        TheWorld.components.cityalarms:ChangeStatus(self.inst.components.citypossession.cityID, true, destroyer)
     end
 
     return unpack(ret)
