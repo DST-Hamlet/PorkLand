7fe476c5ec701fcba62e4b9b37cd4944cf845875
Tony <legendmastertony@gmail.com>
2024-07-25 10:21:03 +0800

    Add oinc sound

--- postinit/prefabs/player.lua
+++ postinit/prefabs/player.lua
@@ -1,6 +1,65 @@
 local AddPlayerPostInit = AddPlayerPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
+local function PlayOincSound(inst)
+    local transaction_amount = inst._oinc_sound:value()
+    if transaction_amount == 0 then
+        return
+    end
+    if transaction_amount == 1 then
+        TheFocalPoint.SoundEmitter:PlaySound("dontstarve_DLC003/common/objects/coins/1")
+    elseif transaction_amount == 2 then
+        TheFocalPoint.SoundEmitter:PlaySound("dontstarve_DLC003/common/objects/coins/2")
+    else
+        TheFocalPoint.SoundEmitter:PlaySound("dontstarve_DLC003/common/objects/coins/3_plus")
+    end
+    inst._oinc_sound:set_local(0)
+end
+
+local function ScheduleOincSoundEvent(inst, amount)
+    if not inst.oinc_transaction then
+        inst.oinc_transaction = 0
+    end
+    inst.oinc_transaction = inst.oinc_transaction + amount
+    if not inst.oinc_transaction_task then
+        inst.oinc_transaction_task = inst:DoTaskInTime(0, function()
+            print("set to", inst.oinc_transaction)
+            inst._oinc_sound:set(inst.oinc_transaction)
+            inst.oinc_transaction = nil
+            inst.oinc_transaction_task = nil
+        end)
+    end
+end
+
+local function OnItemGet(inst, data)
+    local item = data.item
+    if not item or not item:HasTag("oinc") then
+        return
+    end
+
+    ScheduleOincSoundEvent(inst, item.components.stackable and item.components.stackable:StackSize() or 1)
+
+    item.oinc_sound_stackchange_listener = function(_, data)
+        local amount_changed = math.abs(data.stacksize - data.oldstacksize)
+        ScheduleOincSoundEvent(inst, amount_changed)
+    end
+    item:ListenForEvent("stacksizechange", item.oinc_sound_stackchange_listener)
+end
+
+local function OnItemLose(inst, data)
+    local item = data.prev_item
+    if not item or not item:HasTag("oinc") then
+        return
+    end
+
+    ScheduleOincSoundEvent(inst, item.components.stackable and item.components.stackable:StackSize() or 1)
+
+    if item.oinc_sound_stackchange_listener then
+        item:RemoveEventCallback("stacksizechange", item.oinc_sound_stackchange_listener)
+        item.oinc_sound_stackchange_listener = nil
+    end
+end
+
 local function OnDeath(inst, data)
     if inst.components.poisonable ~= nil then
         inst.components.poisonable:SetBlockAll(true)
@@ -52,8 +111,11 @@ end
 AddPlayerPostInit(function(inst)
     if not TheNet:IsDedicated() then
         inst:DoTaskInTime(0, function()
-            if inst == ThePlayer and TheWorld:HasTag("porkland") then --only do this for the local player character
-                inst:AddComponent("windvisuals")
+            if inst == ThePlayer then -- only do this for the local player character
+                inst:ListenForEvent("oincsounddirty", PlayOincSound)
+                if TheWorld:HasTag("porkland") then
+                    inst:AddComponent("windvisuals")
+                end
             end
         end)
     end
@@ -68,6 +130,8 @@ AddPlayerPostInit(function(inst)
         end
     end
 
+    inst._oinc_sound = net_byte(inst.GUID, "player._oincsoundpush", "oincsounddirty")
+
     if not TheWorld.ismastersim then
         return
     end
@@ -81,6 +145,9 @@ AddPlayerPostInit(function(inst)
 
     inst:ListenForEvent("death", OnDeath)
     inst:ListenForEvent("respawnfromghost", OnRespawnFromGhost)
+
+    inst:ListenForEvent("itemget", OnItemGet)
+    inst:ListenForEvent("itemlose", OnItemLose)
 
     if inst.OnLoad then
         inst.Pl_OnLoad = inst.OnLoad
