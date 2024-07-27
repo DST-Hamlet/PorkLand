540f1e6bba8a80f5cb2da9cefb0040b60539e0fb
Cassielu123 <1227984391@qq.com>
2024-07-27 16:04:50 +0800

    add strings

--- strings/tool/input.lua
+++ strings/tool/input.lua
@@ -7,9 +7,7 @@ package.path = package.path .. ";".. dst_string_path .. "/?.lua"
 package.path = package.path .. ";".. ds_path .. "/data/scripts" .. "/?.lua"
 
 keys = {  -- copy key = over key
-    ["OINC10"] = "OINC10",
-    ["OINC100"] = "OINC100",
-    ["OINC"] = "OINC",
+    ["POG"] = "POG",
 }
 
 cn_input_strings = require("string_cn")
