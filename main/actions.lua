b1a3f2daa2146f69d31b9b1aff5aaa55199bdaf8
Tony <legendmastertony@gmail.com>
2024-08-06 11:29:04 +0800

    Move use door code to door component

--- main/actions.lua
+++ main/actions.lua
@@ -270,99 +270,15 @@ ACTIONS.SPY.fn = function(act)
     end
 end
 
-local function DoTeleport(player, pos)
-    player:StartThread(function()
-        local x, y, z = pos:Get()
-
-        -- local invincible = player.components.health.invincible
-        -- player.components.health:SetInvincible(true)
-        if player.components.playercontroller then
-            player.components.playercontroller:EnableMapControls(false)
-            player.components.playercontroller:Enable(false)
-        end
-
-        player:ScreenFade(false, 0.4)
-
-        Sleep(0.4)
-
-        player.Physics:Teleport(x, y, z)
-        player.components.interiorvisitor:UpdateExteriorPos()
-        -- player.components.health:SetInvincible(invincible)
-
-        Sleep(0.1)
-
-        if player.components.playercontroller then
-            player.components.playercontroller:EnableMapControls(true)
-            player.components.playercontroller:Enable(true)
-        end
-
-        if TheWorld.components.interiorspawner:IsInInterior(x, z) then
-            player:SnapCamera()
-        else
-            player.replica.interiorvisitor:RestoreOutsideInteriorCamera()
-        end
-
-        player:ScreenFade(true, 0.4)
-        player.sg:GoToState("idle")
-    end)
-end
-
-local function OnTeleportFailed(player)
-    print("teleportfailed_event failed")
-    -- if player.player_classified then
-    --     player.player_classified.teleportfailed_event:push()
-    -- end
-end
-
-ACTIONS.USEDOOR.fn = function(act, forcesuccess) -- 感觉这里大部分的内容应该移到 component 上去
+ACTIONS.USEDOOR.fn = function(act, forcesuccess)
     local door = act.target
     if not forcesuccess and (door.components.door.disabled or door.components.door.hidden) then
         return false, "DISABLED"
     end
-    local target_interior = door.components.door.target_interior
-    local target_door_id = door.components.door.target_door_id
-
-    local function PlayDoorSound()
-        door:PushEvent("usedoor", {doer = act.doer})
+    local success = door.components.door:Activate(act.doer)
+    if success then
+        return true
     end
-
-    if target_interior == "EXTERIOR" then
-        -- use `target_exterior` firstly, then use current room id as default
-        local id = door.components.door.target_exterior or door.components.door.interior_name
-        local house = TheWorld.components.interiorspawner:GetExteriorById(id)
-        if house then
-            DoTeleport(act.doer, house:GetPosition() + Vector3(house:GetPhysicsRadius(1), 0, 0))
-            PlayDoorSound()
-            act.doer:PushEvent("used_door", {door = door, exterior = true})
-            if house.components.hackable and house.stage > 0 then -- 内部门用 vineable, 外部门用 hackable... 需要代码清理
-                house.stage = 1
-                house.components.hackable:Hack(act.doer, 9999)
-            end
-            return true
-        end
-    else
-        local room = TheWorld.components.interiorspawner:GetInteriorByIndex(target_interior)
-
-        local target_door = room and room:GetDoorById(target_door_id)
-        if target_door then
-            -- don't throw player directly on door
-            -- instead, give a slight offset to room center
-            local door_pos = target_door:GetPosition()
-            local room_pos = room:GetPosition()
-            local offset = (room_pos - door_pos):GetNormalized() * 1.0
-            DoTeleport(act.doer, door_pos + offset)
-            PlayDoorSound()
-            act.doer:PushEvent("used_door", {door = door, exterior = false})
-            if target_door.components.vineable and target_door.components.vineable.vines and
-                target_door.components.vineable.vines.components.hackable and target_door.components.vineable.vines.stage > 0 then
-                    target_door.components.vineable.vines.stage = 1
-                    target_door.components.vineable.vines.components.hackable:Hack(act.doer, 9999)
-            end
-            return true
-        end
-    end
-
-    OnTeleportFailed(act.doer)
     return false, "ERROR"
 end
 
