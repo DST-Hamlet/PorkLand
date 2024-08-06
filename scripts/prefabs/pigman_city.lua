3b1fbdb6e548668d40d0c633ac36bd8956265e74
Tony <legendmastertony@gmail.com>
2024-08-06 19:11:26 +0800

    Trigger greeting on wake for shopkeepers

--- scripts/prefabs/pigman_city.lua
+++ scripts/prefabs/pigman_city.lua
@@ -1097,9 +1097,13 @@ local function shopkeeper_master_postinit(inst)
     inst.separatedesk = separatedesk
     inst.ShopkeeperSpeech = ShopkeeperSpeech
 
+    -- TODO: Make it greet every new customer
     -- TheWorld:ListenForEvent("enterroom", function(data)
     --     inst:ShopkeeperSpeech("CITY_PIG_SHOPKEEPER_GREETING")
     -- end)
+    inst:ListenForEvent("entitywake", function()
+        inst:ShopkeeperSpeech("CITY_PIG_SHOPKEEPER_GREETING")
+    end)
 
     inst:ListenForEvent("death", OnDeath_ShopKeep)
 
