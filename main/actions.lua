14d6385beb901061281798520409f1fbdbb9728a
Tony <legendmastertony@gmail.com>
2024-08-16 16:25:03 +0800

    Fix can't give item to other players Fix #253

--- main/actions.lua
+++ main/actions.lua
@@ -921,17 +921,18 @@ function USEITEM.inventoryitem(inst, doer, target, actions, right, ...)
     if not inst.replica.inventoryitem:CanOnlyGoInPocket() then
         if target:HasTag("weighdownable") then
             table.insert(actions, ACTIONS.WEIGHDOWN)
+            return
         elseif target:HasTag("visual_slot") then
             if target:HasTag("empty") then
                 local shelf = target.replica.visualslot:GetShelf()
                 if not (shelf and shelf.replica.shopped) then
                     table.insert(actions, ACTIONS.PUTONSHELF)
+                    return
                 end
             end
         end
-    else
-        _USEITEM_inventoryitem(inst, doer, target, actions, right, ...)
     end
+    return _USEITEM_inventoryitem(inst, doer, target, actions, right, ...)
 end
 
 local _USEITEM_tool = USEITEM.tool
