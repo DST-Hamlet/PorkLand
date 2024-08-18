6a0ce192bc875827528a2dab453a13dddabf2e15
Tony <legendmastertony@gmail.com>
2024-08-18 15:35:17 +0800

    Make foliage cookable

--- main/cooking.lua
+++ main/cooking.lua
@@ -29,3 +29,5 @@ AddIngredientValues({"jellybug"}, {bug=1}, true)
 AddIngredientValues({"slugbug"}, {bug=1}, true)
 
 AddIngredientValues({"radish", "aloe"}, {veggie=1}, true)
+
+AddIngredientValues({"foliage"}, {veggie=1})
