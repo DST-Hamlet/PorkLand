ea58cd97960c2d52047b38a7d5003cd48a0b9b8a
Ardent <47233045+Noctice@users.noreply.github.com>
2024-08-23 17:08:20 +0800

    add waterfall prefab

--- scripts/prefabs/waterfall_lilypond.lua
+++ scripts/prefabs/waterfall_lilypond.lua
@@ -1,7 +1,16 @@
 local assets =
 {
-    Asset("ANIM", "anim/waterfall_pl.zip"),
+    Asset("ANIM", "anim/waterfall_lilypond.zip"),
+    Asset("ANIM", "anim/waterfall_lilypond_alpha.zip"),
+    Asset("ANIM", "anim/waterfall_lilypond_mix.zip"),
+    Asset("ANIM", "anim/waterfall_lilypond_base.zip"),
 }
+
+local function CreateAlphaChild(inst)
+    local alphachild = SpawnPrefab("waterfall_lilypond_alpha")
+    alphachild.entity:SetParent(inst.entity)
+    inst.waterfall_alpha = alphachild
+end
 
 local function fn()
     local inst = CreateEntity()
@@ -10,15 +19,20 @@ local function fn()
     inst.entity:AddAnimState()
     --[[Non-networked entity]]
 
-    inst.AnimState:SetBuild("waterfall_pl")
+    inst.AnimState:SetBuild("waterfall_lilypond_base")
     inst.AnimState:SetBank("waterfall_pl")
     inst.AnimState:PlayAnimation("idle", false)
     inst.AnimState:SetOrientation(ANIM_ORIENTATION.OnGround)
     inst.AnimState:SetDefaultEffectHandle(resolvefilepath("shaders/anim_waterfall.ksh"))
 
-    inst:DoTaskInTime(0, function()
+    inst.AnimState:SetLayer(LAYER_BELOW_GROUND)
+    inst.AnimState:SetFinalOffset(-2)
+
+    CreateAlphaChild(inst)
+
+    inst:DoPeriodicTask(FRAMES, function()
         local x, _, z = inst.Transform:GetWorldPosition()
-        inst.AnimState:SetFloatParams(x, z, inst.Transform:GetRotation())
+        inst.AnimState:SetFloatParams(x, z, inst.paramrotation or 0)
     end)
 
     inst:AddTag("FX")
@@ -29,4 +43,34 @@ local function fn()
     return inst
 end
 
-return Prefab("waterfall_lilypond", fn, assets)
+local function alpha_fn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    --[[Non-networked entity]]
+
+    inst.AnimState:SetBuild("waterfall_lilypond_base")
+    inst.AnimState:SetBank("waterfall_pl")
+    inst.AnimState:PlayAnimation("idle", false)
+    inst.AnimState:SetOrientation(ANIM_ORIENTATION.OnGround)
+    inst.AnimState:SetDefaultEffectHandle(resolvefilepath("shaders/anim_waterfall_alpha.ksh"))
+
+    inst.AnimState:SetLayer(LAYER_BELOW_GROUND)
+    inst.AnimState:SetFinalOffset(2)
+
+    inst:DoPeriodicTask(FRAMES, function()
+        local x, _, z = inst.Transform:GetWorldPosition()
+        inst.AnimState:SetFloatParams(x, z, inst.paramrotation or 0)
+    end)
+
+    inst:AddTag("FX")
+    inst:AddTag("NOCLICK")
+
+    inst.persists = false
+
+    return inst
+end
+
+return Prefab("waterfall_lilypond", fn, assets),
+    Prefab("waterfall_lilypond_alpha", alpha_fn, assets)
