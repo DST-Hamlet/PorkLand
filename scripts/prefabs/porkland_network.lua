3f82267931df8b0910e44d8e8b3884e8d47ef878
Tony <legendmastertony@gmail.com>
2024-08-25 15:06:55 +0800

    Make compatible with Global Positions

--- scripts/prefabs/porkland_network.lua
+++ scripts/prefabs/porkland_network.lua
@@ -18,6 +18,11 @@ local function custom_postinit(inst)
     inst.components.weather = inst.components.plateauweather
     inst:AddComponent("plateauwind")
     inst:AddComponent("worldplateautemperature")
+
+    -- Compatible with Global Positions https://github.com/rezecib/Global-Positions
+    if softresolvefilepath("scripts/components/globalpositions.lua") then
+        inst:AddComponent("globalpositions")
+    end
 end
 
 return MakeWorldNetwork("porkland_network", prefabs, assets, custom_postinit)
