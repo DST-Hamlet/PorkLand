68c45c8befd96d5d8ba1689b8eecc3d42b54b342
Ziwbi <cd2bfe@gmail.com>
2024-08-25 14:39:49 +0100

    disable earthquake in pig shop Fix #354

--- postinit/components/explosive.lua
+++ postinit/components/explosive.lua
@@ -6,8 +6,8 @@ local _OnBurnt = Explosive.OnBurnt
 function Explosive:OnBurnt()
     local interiorID = self.inst:GetCurrentInteriorID()
     if interiorID then
-        local name = TheWorld.components.interiorspawner.interiors[interiorID].dungeon_name
-        if not name or not name:find("pig_shop") then -- maybe we should define interior quake rooms as constants instead?
+        local name = TheWorld.components.interiorspawner.interior_defs[interiorID].dungeon_name
+        if name and not (name:find("pig_shop") or name:find("playerhouse"))then -- maybe we should define interior quake rooms as constants instead?
             TheWorld:PushEvent("interior_startquake", {interiorID = interiorID, quake_level = INTERIOR_QUAKE_LEVELS.PILLAR_WORKED})
         end
     end
