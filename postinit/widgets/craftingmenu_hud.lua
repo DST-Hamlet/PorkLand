cae119b0f7b96ece75007380b51eead093284e98
jerry <2234834467@qq.com>
2024-08-27 01:05:04 +0800

    fix bug

--- postinit/widgets/craftingmenu_hud.lua
+++ postinit/widgets/craftingmenu_hud.lua
@@ -2,6 +2,6 @@ local AddClassPostConstruct = AddClassPostConstruct
 GLOBAL.setfenv(1, GLOBAL)
 
 AddClassPostConstruct("widgets/redux/craftingmenu_hud", function(self)
-    self.inst:ListenForEvent("enterinterior", function() self:UpdateRecipes() end, self.owner)
-    self.inst:ListenForEvent("leaveinterior", function() self:UpdateRecipes() end, self.owner)
+    self.inst:ListenForEvent("enterinterior_client", function() self:UpdateRecipes() end, self.owner)
+    self.inst:ListenForEvent("leaveinterior_client", function() self:UpdateRecipes() end, self.owner)
 end)
