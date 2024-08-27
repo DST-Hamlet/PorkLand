4879732c742a066f79a6f54041520ecc23959b33
Ardent <47233045+Noctice@users.noreply.github.com>
2024-08-27 18:27:41 +0800

    优化了草长成高草的机制

--- postinit/prefabs/grass.lua
+++ postinit/prefabs/grass.lua
@@ -5,6 +5,7 @@ AddPrefabPostInit("grass", function(inst)
     if not TheWorld.ismastersim then
         return
     end
+
     local _onregenfn = inst.components.pickable.onregenfn
     local function onregenfn(inst, ...)
         local x, y, z = inst.Transform:GetWorldPosition()
@@ -19,4 +20,41 @@ AddPrefabPostInit("grass", function(inst)
         _onregenfn(inst, ...)
     end
     inst.components.pickable.onregenfn = onregenfn
+
+    local _ontransplantfn = inst.components.pickable.ontransplantfn
+    local function ontransplantfn(inst, ...)
+        local x, y, z = inst.Transform:GetWorldPosition()
+        local tile = TheWorld.Map:GetTileAtPoint(x, y, z)
+        if NUTRIENT_TILES[tile] then
+            local tallgrass = ReplacePrefab(inst, "grass_tall")
+            tallgrass.components.hackable:MakeBarren()
+            return
+        end
+        _ontransplantfn(inst, ...)
+    end
+    inst.components.pickable.ontransplantfn = ontransplantfn
+end)
+
+AddPrefabPostInit("dug_grass_placer", function(inst)
+    local _OnUpdate = inst.components.placer.OnUpdate
+    function inst.components.placer:OnUpdate(dt, ...)
+        _OnUpdate(self, dt, ...)
+        local x, y, z = inst.Transform:GetWorldPosition()
+        local tile = TheWorld.Map:GetTileAtPoint(x, y, z)
+        if NUTRIENT_TILES[tile] then
+            if not self.istall then
+                self.istall = true
+                self.inst.AnimState:SetBank("grass_tall")
+                self.inst.AnimState:SetBuild("grass_tall")
+                self.inst.AnimState:PlayAnimation("idle", true)
+            end
+        else
+            if self.istall then
+                self.istall = false
+                self.inst.AnimState:SetBank("grass")
+                self.inst.AnimState:SetBuild("grass1")
+                self.inst.AnimState:PlayAnimation("idle", true)
+            end
+        end
+    end
 end)
