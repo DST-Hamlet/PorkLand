21ddddeb0d5e7b64b0fac9b682798653e9c7f092
Ardent <47233045+Noctice@users.noreply.github.com>
2024-09-13 07:14:09 +0800

    修复了名为antqueen的InteriorTag没有被正确添加的问题

--- scripts/prefabs/interiorworkblank.lua
+++ scripts/prefabs/interiorworkblank.lua
@@ -339,7 +339,7 @@ local TAGS = {
     HOME_PROTOTYPER = 8, -- player home prototype room
     PIG_RUINS = 16, -- pig ruins rooms
     PIG_SHOP = 32, -- pig shop rooms
-    antqueen = 64, -- final chamber
+    ANTQUEEN = 64, -- final chamber
     TEST = 1024,
 }
 
