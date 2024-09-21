268efd20c2fa175a42b76f24fdbdbceb1191059b
Ardent <47233045+Noctice@users.noreply.github.com>
2024-09-21 22:39:42 +0800

    优化了与旧版本排队论的冲突

--- postinit/components/actionqueuer.lua
+++ postinit/components/actionqueuer.lua
@@ -3,8 +3,10 @@ GLOBAL.setfenv(1, GLOBAL)
 
 -- for client mod ActionQueue
 AddComponentPostInit("actionqueuer", function(self)
-    self.AddActionList("allclick", "SHEAR", "HACK", "PAN", "DISLODGE")
-    self.AddActionList("leftclick", "SHEAR", "HACK", "PAN", "REPAIRBOAT", "RETRIEVE", "DISLODGE")
-    self.AddActionList("autocollect", "SHEAR", "HACK", "PAN", "DISLODGE")
-    self.AddActionList("noworkdelay", "SHEAR", "HACK", "PAN", "DISLODGE")
+    if self.AddActionList then
+        self.AddActionList("allclick", "SHEAR", "HACK", "PAN", "DISLODGE")
+        self.AddActionList("leftclick", "SHEAR", "HACK", "PAN", "REPAIRBOAT", "RETRIEVE", "DISLODGE")
+        self.AddActionList("autocollect", "SHEAR", "HACK", "PAN", "DISLODGE")
+        self.AddActionList("noworkdelay", "SHEAR", "HACK", "PAN", "DISLODGE")
+    end
 end)
