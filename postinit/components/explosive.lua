99e9928810fd6e233dd2bae46d4000246ea489d0
Tony <legendmastertony@gmail.com>
2024-10-05 11:04:02 +0800

    Fix #665

--- postinit/components/explosive.lua
+++ postinit/components/explosive.lua
@@ -12,7 +12,7 @@ function Explosive:OnBurnt()
     local interiorID = self.inst:GetCurrentInteriorID()
     if interiorID then
         local name = TheWorld.components.interiorspawner.interior_defs[interiorID].dungeon_name
-        if name and not (name:find("pig_shop") or name:find("playerhouse"))then -- maybe we should define interior quake rooms as constants instead?
+        if name and not (name:find("pig_shop") or name:find("playerhouse") or name == "pig_palace") then -- maybe we should define interior quake rooms as constants instead?
             TheWorld:PushEvent("interior_startquake", {interiorID = interiorID, quake_level = INTERIOR_QUAKE_LEVELS.PILLAR_WORKED})
         end
     end
