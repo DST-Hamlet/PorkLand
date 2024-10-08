b80b34998da0a87c2c33bc3482e1dd6645bba773
Ardent <47233045+Noctice@users.noreply.github.com>
2024-10-08 21:29:57 +0800

    在拿起一个呼噜币时不会触发两个呼噜币的音效

--- postinit/prefabs/player.lua
+++ postinit/prefabs/player.lua
@@ -4,9 +4,8 @@ GLOBAL.setfenv(1, GLOBAL)
 local function PlayOincSound(inst)
     local transaction_amount = inst._oinc_sound:value()
     if transaction_amount == 0 then
-        return
-    end
-    if transaction_amount == 1 then
+        TheFocalPoint.SoundEmitter:PlaySound("dontstarve_DLC003/common/objects/coins/1")
+    elseif transaction_amount == 1 then
         TheFocalPoint.SoundEmitter:PlaySound("dontstarve_DLC003/common/objects/coins/1")
     elseif transaction_amount == 2 then
         TheFocalPoint.SoundEmitter:PlaySound("dontstarve_DLC003/common/objects/coins/2")
@@ -19,11 +18,19 @@ local function ScheduleOincSoundEvent(inst, amount)
     if not inst.oinc_transaction then
         inst.oinc_transaction = 0
     end
+    if not inst.max_oinc_transaction then
+        inst.max_oinc_transaction = 0
+    end
     inst.oinc_transaction = inst.oinc_transaction + amount
+    if math.abs(inst.oinc_transaction) > inst.max_oinc_transaction then
+        inst.max_oinc_transaction = math.abs(inst.oinc_transaction)
+    end
+
     if not inst.oinc_transaction_task then
         inst.oinc_transaction_task = inst:DoTaskInTime(0, function()
             inst._oinc_sound:set_local(0)
-            inst._oinc_sound:set(inst.oinc_transaction)
+            inst._oinc_sound:set(inst.max_oinc_transaction)
+            inst.max_oinc_transaction = nil
             inst.oinc_transaction = nil
             inst.oinc_transaction_task = nil
         end)
@@ -40,7 +47,7 @@ local function OnItemGet(inst, data)
         ScheduleOincSoundEvent(inst, item.components.stackable and item.components.stackable:StackSize() or 1)
 
         item.oinc_sound_stackchange_listener = function(_, data)
-            local amount_changed = math.abs(data.stacksize - data.oldstacksize)
+            local amount_changed = data.stacksize - data.oldstacksize
             ScheduleOincSoundEvent(inst, amount_changed)
         end
         item:ListenForEvent("stacksizechange", item.oinc_sound_stackchange_listener)
@@ -55,7 +62,7 @@ local function OnItemLose(inst, data)
     end
 
     if item:HasTag("oinc") then
-        ScheduleOincSoundEvent(inst, item.components.stackable and item.components.stackable:StackSize() or 1)
+        ScheduleOincSoundEvent(inst, item.components.stackable and (- item.components.stackable:StackSize()) or - 1)
 
         if item.oinc_sound_stackchange_listener then
             item:RemoveEventCallback("stacksizechange", item.oinc_sound_stackchange_listener)
