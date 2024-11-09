48ba4e7f477adb7672d53a94bfe049dd48b66565
Tony <legendmastertony@gmail.com>
2024-11-09 16:31:48 +0800

    Record SetDrawOverFogOfWar

--- postinit/minimapentity.lua
+++ postinit/minimapentity.lua
@@ -4,12 +4,28 @@ local icon_name = {}
 local icon_priority = {}
 local icon_can_use_cache = {}
 local icon_offset = {}
+local icon_can_draw_over_fog = {}
+local minimap_entity_to_entity = {}
 
-function PorkLandOnMiniMapEntityRemove(inst)
+local function cleanup_mapping(inst)
     icon_name[inst.MiniMapEntity] = nil
     icon_priority[inst.MiniMapEntity] = nil
     icon_can_use_cache[inst.MiniMapEntity] = nil
     icon_offset[inst.MiniMapEntity] = nil
+    icon_can_draw_over_fog[inst.MiniMapEntity] = nil
+    minimap_entity_to_entity[inst.MiniMapEntity] = nil
+end
+
+local add_minimap_entity = Entity.AddMiniMapEntity
+function Entity:AddMiniMapEntity(...)
+    local minimap_entity = add_minimap_entity(self, ...)
+
+    local guid = self:GetGUID()
+    local inst = Ents[guid]
+    minimap_entity_to_entity[minimap_entity] = inst
+    inst:ListenForEvent("onremove", cleanup_mapping)
+
+    return minimap_entity
 end
 
 local _SetIcon = MiniMapEntity.SetIcon
@@ -41,6 +57,16 @@ end
 function MiniMapEntity:GetCanUseCache()
     -- Cacheable by default, so nil is true
     return icon_can_use_cache[self] ~= false
+end
+
+local set_draw_over_fog_of_war = MiniMapEntity.SetDrawOverFogOfWar
+function MiniMapEntity:SetDrawOverFogOfWar(can_draw_over, ...)
+    icon_can_draw_over_fog[self] = can_draw_over
+    return set_draw_over_fog_of_war(self, can_draw_over, ...)
+end
+
+function MiniMapEntity:GetCanDrawOverFogOfWar()
+    return icon_can_draw_over_fog[self]
 end
 
 function MiniMapEntity:SetIconOffset(x, y)
