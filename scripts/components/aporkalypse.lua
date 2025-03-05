bafe20a8555deca931520ef0a9391c9684f5101c
MonkeyAgainstHeaven <yreorel@gmail.com>
2025-03-05 15:49:18 +0800

    Update aporkalypse.lua

--- scripts/components/aporkalypse.lua
+++ scripts/components/aporkalypse.lua
@@ -329,12 +329,18 @@ return Class(function(self, inst)
             _herald_time = _herald_time - dt
             if _herald_time <= 0 then
                 local players = {}
+                local oldest_age = -1
+                local oldest_player = nil
                 for _, player in pairs(AllPlayers) do
-                    if not player:GetIsInInterior() and player.components.age:GetAgeInDays() < 15 then
+                    local age = player.components.age:GetAgeInDays()
+                    if not player:GetIsInInterior() and (player.PL_CausedAporkalypse == true or age >= 15) then
                         table.insert(players, player)
+                    elseif age > oldest_age then
+                        oldest_player = player
+                        oldest_age = age
                     end
                 end
-
+                if players[1] == nil then players[1] = oldest_player end
                 local player = GetRandomItem(players)
                 SpawnHerald(player)
                 ScheduleHeraldAttack()
