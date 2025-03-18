b5e176c29a1f6145c8bddaba2e1edb3d5e738087
Ardent <1260405837@qq.com>
2025-03-18 20:06:29 +0800

    基础的边缘效果

--- scripts/components/pl_tilemanager.lua
+++ scripts/components/pl_tilemanager.lua
@@ -23,6 +23,79 @@ function PL_TileManager:OnRemoveEntity()
     self:ClearTiles()
 end
 
+local WEST = 1
+local NORTH_WEST = 2
+local NORTH = 4
+local NORTH_EAST = 8
+local EAST = 16
+local SOUTH_EAST = 32
+local SOUTH = 64
+local SOUTH_WEST = 128
+
+local NEIGHBOR_TILES = {
+    [WEST]       = {x = -1, z =  0},
+    [NORTH_WEST] = {x = -1, z =  1},
+    [NORTH]      = {x =  0, z =  1},
+    [NORTH_EAST] = {x =  1, z =  1},
+    [EAST]       = {x =  1, z =  0},
+    [SOUTH_EAST] = {x =  1, z = -1},
+    [SOUTH]      = {x =  0, z = -1},
+    [SOUTH_WEST] = {x = -1, z = -1},
+}
+
+local function GetKeyForNeighbors(data)
+    local neighbors = {}
+    for i, dir in ipairs(data) do
+        neighbors[dir] = true
+    end
+
+    if neighbors[WEST] then
+        neighbors[NORTH_WEST] = true
+        neighbors[SOUTH_WEST] = true
+    end
+    if neighbors[NORTH] then
+        neighbors[NORTH_WEST] = true
+        neighbors[NORTH_EAST] = true
+    end
+    if neighbors[EAST] then
+        neighbors[NORTH_EAST] = true
+        neighbors[SOUTH_EAST] = true
+    end
+    if neighbors[SOUTH] then
+        neighbors[SOUTH_WEST] = true
+        neighbors[SOUTH_EAST] = true
+    end
+
+    local key = 0
+    for k, v in pairs(neighbors) do
+        key = key + k
+    end
+
+    return key
+end
+
+tile_map = {}
+
+local function AddToTileMap(val, data)
+    local key = GetKeyForNeighbors(data)
+    tile_map[key] = val
+end
+AddToTileMap(2, {WEST})
+AddToTileMap(3, {NORTH})
+AddToTileMap(4, {NORTH, WEST})
+AddToTileMap(5, {EAST})
+AddToTileMap(6, {EAST, WEST})
+AddToTileMap(7, {EAST, NORTH})
+AddToTileMap(8, {EAST, NORTH, WEST})
+AddToTileMap(9, {SOUTH})
+AddToTileMap(10, {SOUTH, WEST})
+AddToTileMap(11, {SOUTH, NORTH})
+AddToTileMap(12, {SOUTH, NORTH, WEST})
+AddToTileMap(13, {SOUTH, EAST})
+AddToTileMap(14, {SOUTH, EAST, WEST})
+AddToTileMap(15, {SOUTH, EAST, NORTH})
+AddToTileMap(16, {SOUTH, EAST, NORTH, WEST})
+
 PL_TileManager.OnRemoveFromEntity = PL_TileManager.OnRemoveEntity
 
 function PL_TileManager:UpdateTiles()
@@ -33,33 +106,23 @@ function PL_TileManager:UpdateTiles()
             local center = current_tile_center + Vector3(x * TILE_SCALE, 0, z * TILE_SCALE)
             local tile = TheWorld.Map:GetTileAtPoint(center.x, center.y, center.z)
             if tile then
-                -- local adjacent_tiles = {
-                --     {false, false, false},
-                --     {false, false, false},
-                --     {false, false, false},
-                -- }
-                local value = 0
-                local bitmask_values = {
-                    { 1,   2,   4   },
-                    { 8,   16,  32  },
-                    { 64,  128, 256 },
-                }
-                local tile_map = {
-                    [0] = 17,
-                    [1] = 18,
-                    [16] = 1,
-                }
-                for x = -1, 1 do
-                    for z = -1, 1 do
-                        local adjacent_tile = TheWorld.Map:GetTileAtPoint(center.x + x * TILE_SCALE, center.y, center.z + z * TILE_SCALE)
-                        -- adjacent_tiles[x + 2][z + 2] = adjacent_tile == WORLD_TILES.LILYPOND
+                if tile == WORLD_TILES.LILYPOND then
+                    self.tiletest:SpawnTile(center, 1)
+                else
+                    local neighbor_data = {}
+                    for dir, v in pairs(NEIGHBOR_TILES) do
+                        local adjacent_tile = TheWorld.Map:GetTileAtPoint(center.x + v.x * TILE_SCALE, center.y, center.z + v.z * TILE_SCALE)
                         if adjacent_tile == WORLD_TILES.LILYPOND then
-                            value = value + bitmask_values[z + 2][x + 2]
+                            table.insert(neighbor_data, dir)
                         end
                     end
-                end
 
-                self.tiletest:SpawnTile(center, tile_map[value] or 17)
+                    local key = GetKeyForNeighbors(neighbor_data)
+
+                    if tile_map[key] then
+                        self.tiletest:SpawnTile(center, tile_map[key] or 17)
+                    end
+                end
             end
         end
     end
