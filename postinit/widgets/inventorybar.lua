aace11b7ad04aac252f65e128b8105c65fb0c7c7
Ardent <1260405837@qq.com>
2025-04-16 19:23:35 +0800

    缩短了薇勒尔的物品栏背景宽度

--- postinit/widgets/inventorybar.lua
+++ postinit/widgets/inventorybar.lua
@@ -61,6 +61,10 @@ local function RebuildLayout(self, inventory, overflow, do_integrated_backpack, 
     end
 
     _RebuildLayout(self, inventory, overflow, do_integrated_backpack, do_self_inspect, ...)
+
+    local bg_scale = (1.15 * total_w) / (1480) -- This is a bit ugly, the hardcoded 1480 is the standard width for a regular inventory total_w
+    self.bg:SetScale(bg_scale, 1, 1)
+    self.bgcover:SetScale(bg_scale, 1, 1)
 end
 
 ToolUtil.SetUpvalue(InventoryBar.Rebuild, RebuildLayout, "RebuildLayout")
