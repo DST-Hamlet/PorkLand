14e0be397713bb91e1d8816190f7585413d969fa
Tony <legendmastertony@gmail.com>
2025-04-29 14:26:37 +0800

    Support keyboard shortcut use item

--- postinit/widgets/invslot.lua
+++ postinit/widgets/invslot.lua
@@ -14,14 +14,16 @@ function InvSlot:UseItem(...)
             if not TheInput:ControllerAttached() and SUPPORTED_ITEMS[item.prefab] and item.components.spellbook then
                 local action = ThePlayer.components.playeractionpicker:GetInventoryActions(item)[1]
                 -- print(action)
-                if action.action == ACTIONS.USESPELLBOOK then
-                    -- print("ThePlayer.HUD.controls.spellcontrols:Open()")
-                    ThePlayer.HUD.controls.spellcontrols:Open(item.components.spellbook.items, item, self.tile:GetWorldPosition())
-                    return
-                end
-                if action.action == ACTIONS.CLOSESPELLBOOK then
-                    ThePlayer.HUD.controls.spellcontrols:Close()
-                    return
+                if action then
+                    if action.action == ACTIONS.USESPELLBOOK then
+                        -- print("ThePlayer.HUD.controls.spellcontrols:Open()")
+                        ThePlayer.HUD.controls.spellcontrols:Open(item.components.spellbook.items, item, self.tile:GetWorldPosition())
+                        return
+                    end
+                    if action.action == ACTIONS.CLOSESPELLBOOK then
+                        ThePlayer.HUD.controls.spellcontrols:Close()
+                        return
+                    end
                 end
             end
 
