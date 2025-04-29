17d6fd7a5500918e8fd99acf3e08da17382bafea
Tony <legendmastertony@gmail.com>
2025-04-29 15:04:15 +0800

    Remove attack at, escape and scare commands

--- postinit/prefabs/abigail_flower.lua
+++ postinit/prefabs/abigail_flower.lua
@@ -53,17 +53,17 @@ local function DoGhostSpell(doer, event, state, ...)
 	return true
 end
 
-local function GhostEscapeSpell(inst, doer)
-	return DoGhostSpell(doer, "do_ghost_escape")
-end
+-- local function GhostEscapeSpell(inst, doer)
+-- 	return DoGhostSpell(doer, "do_ghost_escape")
+-- end
 
-local function GhostAttackAtSpell(inst, doer, pos)
-	return DoGhostSpell(doer, "do_ghost_attackat", nil, pos)
-end
+-- local function GhostAttackAtSpell(inst, doer, pos)
+-- 	return DoGhostSpell(doer, "do_ghost_attackat", nil, pos)
+-- end
 
-local function GhostScareSpell(inst, doer)
-	return DoGhostSpell(doer, nil, "scare")
-end
+-- local function GhostScareSpell(inst, doer)
+-- 	return DoGhostSpell(doer, nil, "scare")
+-- end
 
 local function GhostHauntSpell(inst, doer, pos)
 	return DoGhostSpell(doer, "do_ghost_hauntat", nil, pos)
@@ -170,128 +170,128 @@ local COMMANDS = {
         },
         widget_scale = ICON_SCALE,
     },
-	{
-		label = STRINGS.GHOSTCOMMANDS.ESCAPE,
-		onselect = function(inst)
-			local spellbook = inst.components.spellbook
-			spellbook:SetSpellName(STRINGS.GHOSTCOMMANDS.ESCAPE)
-			spellbook:SetSpellAction(nil)
+	-- {
+	-- 	label = STRINGS.GHOSTCOMMANDS.ESCAPE,
+	-- 	onselect = function(inst)
+	-- 		local spellbook = inst.components.spellbook
+	-- 		spellbook:SetSpellName(STRINGS.GHOSTCOMMANDS.ESCAPE)
+	-- 		spellbook:SetSpellAction(nil)
 
-			if TheWorld.ismastersim then
-				inst.components.aoespell:SetSpellFn(nil)
-                spellbook:SetSpellFn(GhostEscapeSpell)
-			end
-		end,
-		execute = function(inst)
-			if ThePlayer.replica.inventory then
-				ThePlayer.replica.inventory:CastSpellBookFromInv(inst)
-			end
-		end,
-		bank = "spell_icons_wendy",
-		build = "spell_icons_wendy",
-		anims =
-		{
-			idle = { anim = "teleport" },
-			focus = { anim = "teleport_focus", loop = true },
-			down = { anim = "teleport_pressed" },
-			disabled = { anim = "teleport_disabled" },
-			cooldown = { anim = "teleport_cooldown" },
-		},
-		widget_scale = ICON_SCALE,
-		checkcooldown = function(doer)
-			--client safe
-			return (doer ~= nil
-				and doer.components.spellbookcooldowns
-				and doer.components.spellbookcooldowns:GetSpellCooldownPercent("ghostcommand"))
-				or nil
-		end,
-		cooldowncolor = { 0.65, 0.65, 0.65, 0.75 },
-	},
-    {
-        label = STRINGS.GHOSTCOMMANDS.ATTACK_AT,
-        onselect = function(inst)
-			local spellbook = inst.components.spellbook
-			local aoetargeting = inst.components.aoetargeting
+	-- 		if TheWorld.ismastersim then
+	-- 			inst.components.aoespell:SetSpellFn(nil)
+    --             spellbook:SetSpellFn(GhostEscapeSpell)
+	-- 		end
+	-- 	end,
+	-- 	execute = function(inst)
+	-- 		if ThePlayer.replica.inventory then
+	-- 			ThePlayer.replica.inventory:CastSpellBookFromInv(inst)
+	-- 		end
+	-- 	end,
+	-- 	bank = "spell_icons_wendy",
+	-- 	build = "spell_icons_wendy",
+	-- 	anims =
+	-- 	{
+	-- 		idle = { anim = "teleport" },
+	-- 		focus = { anim = "teleport_focus", loop = true },
+	-- 		down = { anim = "teleport_pressed" },
+	-- 		disabled = { anim = "teleport_disabled" },
+	-- 		cooldown = { anim = "teleport_cooldown" },
+	-- 	},
+	-- 	widget_scale = ICON_SCALE,
+	-- 	checkcooldown = function(doer)
+	-- 		--client safe
+	-- 		return (doer ~= nil
+	-- 			and doer.components.spellbookcooldowns
+	-- 			and doer.components.spellbookcooldowns:GetSpellCooldownPercent("ghostcommand"))
+	-- 			or nil
+	-- 	end,
+	-- 	cooldowncolor = { 0.65, 0.65, 0.65, 0.75 },
+	-- },
+    -- {
+    --     label = STRINGS.GHOSTCOMMANDS.ATTACK_AT,
+    --     onselect = function(inst)
+	-- 		local spellbook = inst.components.spellbook
+	-- 		local aoetargeting = inst.components.aoetargeting
 
-            spellbook:SetSpellName(STRINGS.GHOSTCOMMANDS.ATTACK_AT)
-			spellbook:SetSpellAction(nil)
-            aoetargeting:SetDeployRadius(0)
-			aoetargeting:SetRange(20)
-            aoetargeting.reticule.reticuleprefab = "reticuleaoeghosttarget"
-            aoetargeting.reticule.pingprefab = "reticuleaoeghosttarget_ping"
+    --         spellbook:SetSpellName(STRINGS.GHOSTCOMMANDS.ATTACK_AT)
+	-- 		spellbook:SetSpellAction(nil)
+    --         aoetargeting:SetDeployRadius(0)
+	-- 		aoetargeting:SetRange(20)
+    --         aoetargeting.reticule.reticuleprefab = "reticuleaoeghosttarget"
+    --         aoetargeting.reticule.pingprefab = "reticuleaoeghosttarget_ping"
 
-            aoetargeting.reticule.mousetargetfn = nil
-            aoetargeting.reticule.targetfn = ReticuleGhostTargetFn
-            aoetargeting.reticule.updatepositionfn = nil
-			aoetargeting.reticule.twinstickrange = 15
+    --         aoetargeting.reticule.mousetargetfn = nil
+    --         aoetargeting.reticule.targetfn = ReticuleGhostTargetFn
+    --         aoetargeting.reticule.updatepositionfn = nil
+	-- 		aoetargeting.reticule.twinstickrange = 15
 
-            if TheWorld.ismastersim then
-                aoetargeting:SetTargetFX("reticuleaoeghosttarget")
-                inst.components.aoespell:SetSpellFn(GhostAttackAtSpell)
-                spellbook:SetSpellFn(nil)
-            end
-        end,
-        execute = StartAOETargeting,
-		bank = "spell_icons_wendy",
-		build = "spell_icons_wendy",
-		anims =
-		{
-			idle = { anim = "attack_at" },
-			focus = { anim = "attack_at_focus", loop = true },
-			down = { anim = "attack_at_pressed" },
-			disabled = { anim = "attack_at_disabled" },
-			cooldown = { anim = "attack_at_cooldown" },
-		},
-        widget_scale = ICON_SCALE,
-		checkcooldown = function(doer)
-			--client safe
-			if doer == nil or doer.components.spellbookcooldowns == nil then
-				return
-			end
+    --         if TheWorld.ismastersim then
+    --             aoetargeting:SetTargetFX("reticuleaoeghosttarget")
+    --             inst.components.aoespell:SetSpellFn(GhostAttackAtSpell)
+    --             spellbook:SetSpellFn(nil)
+    --         end
+    --     end,
+    --     execute = StartAOETargeting,
+	-- 	bank = "spell_icons_wendy",
+	-- 	build = "spell_icons_wendy",
+	-- 	anims =
+	-- 	{
+	-- 		idle = { anim = "attack_at" },
+	-- 		focus = { anim = "attack_at_focus", loop = true },
+	-- 		down = { anim = "attack_at_pressed" },
+	-- 		disabled = { anim = "attack_at_disabled" },
+	-- 		cooldown = { anim = "attack_at_cooldown" },
+	-- 	},
+    --     widget_scale = ICON_SCALE,
+	-- 	checkcooldown = function(doer)
+	-- 		--client safe
+	-- 		if doer == nil or doer.components.spellbookcooldowns == nil then
+	-- 			return
+	-- 		end
 
-			local cooldown = math.max(doer.components.spellbookcooldowns:GetSpellCooldownPercent("do_ghost_attackat") or 0, doer.components.spellbookcooldowns:GetSpellCooldownPercent("ghostcommand") or 0)
+	-- 		local cooldown = math.max(doer.components.spellbookcooldowns:GetSpellCooldownPercent("do_ghost_attackat") or 0, doer.components.spellbookcooldowns:GetSpellCooldownPercent("ghostcommand") or 0)
 
-			return cooldown > 0 and cooldown or nil
-		end,
-		cooldowncolor = { 0.65, 0.65, 0.65, 0.75 },
-    },
-    {
-        label = STRINGS.GHOSTCOMMANDS.SCARE,
-        onselect = function(inst)
-            local spellbook = inst.components.spellbook
-            spellbook:SetSpellName(STRINGS.GHOSTCOMMANDS.SCARE)
-            spellbook:SetSpellAction(nil)
+	-- 		return cooldown > 0 and cooldown or nil
+	-- 	end,
+	-- 	cooldowncolor = { 0.65, 0.65, 0.65, 0.75 },
+    -- },
+    -- {
+    --     label = STRINGS.GHOSTCOMMANDS.SCARE,
+    --     onselect = function(inst)
+    --         local spellbook = inst.components.spellbook
+    --         spellbook:SetSpellName(STRINGS.GHOSTCOMMANDS.SCARE)
+    --         spellbook:SetSpellAction(nil)
 
-            if TheWorld.ismastersim then
-                inst.components.aoespell:SetSpellFn(nil)
-                spellbook:SetSpellFn(GhostScareSpell)
-            end
-        end,
-        execute = function(inst)
-            if ThePlayer.replica.inventory then
-                ThePlayer.replica.inventory:CastSpellBookFromInv(inst)
-            end
-        end,
-        bank = "spell_icons_wendy",
-        build = "spell_icons_wendy",
-        anims =
-        {
-            idle = { anim = "scare" },
-            focus = { anim = "scare_focus", loop = true },
-            down = { anim = "scare_pressed" },
-            disabled = { anim = "scare_disabled" },
-            cooldown = { anim = "scare_cooldown" },
-        },
-        widget_scale = ICON_SCALE,
-        checkcooldown = function(doer)
-            --client safe
-            return (doer ~= nil
-                and doer.components.spellbookcooldowns
-                and doer.components.spellbookcooldowns:GetSpellCooldownPercent("ghostcommand"))
-                or nil
-        end,
-        cooldowncolor = { 0.65, 0.65, 0.65, 0.75 },
-    },
+    --         if TheWorld.ismastersim then
+    --             inst.components.aoespell:SetSpellFn(nil)
+    --             spellbook:SetSpellFn(GhostScareSpell)
+    --         end
+    --     end,
+    --     execute = function(inst)
+    --         if ThePlayer.replica.inventory then
+    --             ThePlayer.replica.inventory:CastSpellBookFromInv(inst)
+    --         end
+    --     end,
+    --     bank = "spell_icons_wendy",
+    --     build = "spell_icons_wendy",
+    --     anims =
+    --     {
+    --         idle = { anim = "scare" },
+    --         focus = { anim = "scare_focus", loop = true },
+    --         down = { anim = "scare_pressed" },
+    --         disabled = { anim = "scare_disabled" },
+    --         cooldown = { anim = "scare_cooldown" },
+    --     },
+    --     widget_scale = ICON_SCALE,
+    --     checkcooldown = function(doer)
+    --         --client safe
+    --         return (doer ~= nil
+    --             and doer.components.spellbookcooldowns
+    --             and doer.components.spellbookcooldowns:GetSpellCooldownPercent("ghostcommand"))
+    --             or nil
+    --     end,
+    --     cooldowncolor = { 0.65, 0.65, 0.65, 0.75 },
+    -- },
     {
         label = STRINGS.GHOSTCOMMANDS.HAUNT_AT,
         onselect = function(inst)
@@ -335,7 +335,7 @@ local COMMANDS = {
                 or nil
         end,
         cooldowncolor = { 0.65, 0.65, 0.65, 0.75 },
-    }
+    },
 }
 
 AddPrefabPostInit("abigail_flower", function(inst)
