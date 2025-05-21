5680e9505d00d4772b59747cb765500436a375da
Tony <legendmastertony@gmail.com>
2025-05-21 14:25:31 +0800

    Initial setup for interior hud indicatables

---
+++ scripts/prefabs/target_indicator_marker.lua
@@ -0,0 +1,34 @@
+local function RemoveHudIndicator(inst)
+	if ThePlayer and ThePlayer.HUD then
+		ThePlayer.HUD:RemoveTargetIndicator(inst)
+	end
+end
+
+local function SetupHudIndicator(inst)
+    if ThePlayer then
+        ThePlayer.HUD:AddTargetIndicator(inst, {image = "avatar_megaflare.tex"})
+        inst:ListenForEvent("onremove", RemoveHudIndicator)
+    end
+end
+
+local function fn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    --[[Non-networked entity]]
+
+    inst.entity:SetCanSleep(false)
+
+    inst:AddTag("CLASSIFIED")
+
+    -- We set this data from `InteriorHudIndicatableManager`
+    inst.marker_data = nil
+
+    inst:DoTaskInTime(0, SetupHudIndicator)
+
+    inst.persists = false
+
+    return inst
+end
+
+return Prefab("target_indicator_marker", fn)
