0316bcbe95ed923b70eb8d4ba21aae31635bae6e
Tony <legendmastertony@gmail.com>
2025-05-22 20:30:11 +0800

    Add position calculation for targetindicator

--- postinit/widgets/targetindicator.lua
+++ postinit/widgets/targetindicator.lua
@@ -3,6 +3,27 @@ GLOBAL.setfenv(1, GLOBAL)
 
 local TargetIndicator = require("widgets/targetindicator")
 
+local INTERIOR_SPACEING = 20
+
+local MIN_SCALE = .5
+-- local MIN_ALPHA = .35
+
+local DEFAULT_ATLAS = "images/avatars.xml"
+local DEFAULT_AVATAR = "avatar_unknown.tex"
+
+-- TODO: Combine this with the one inside `postinit/widgets/mapwidget.lua`
+-- and also use it for the `wheeler_tracker`
+local function CalculateOffset(current_center, target_x, target_y)
+    -- Convert the grid coordinates to positions
+    -- as a compromise, we don't know the exact position,
+    -- just use the current room's size as an estimate
+    local current_x, current_y = current_center:GetCoordinates()
+    local width, depth = current_center:GetSize()
+    local offset_x = (target_x - current_x) * (width + INTERIOR_SPACEING * 2)
+    local offset_y = (target_y - current_y) * (depth + INTERIOR_SPACEING * 2)
+    return Vector3(-offset_y, 0, offset_x)
+end
+
 local on_update = TargetIndicator.OnUpdate
 function TargetIndicator:OnUpdate(...)
     if TheNet:IsServerPaused() then return end
@@ -26,7 +47,17 @@ function TargetIndicator:OnUpdate(...)
         self.head:SetTexture(self:GetAvatarAtlas(), self:GetAvatar(), DEFAULT_AVATAR)
     end
 
-    -- TODO
+    local center = TheWorld.components.interiorspawner:GetInteriorCenter(self.owner:GetPosition())
+    if not center then
+        return
+    end
+
+    local coord_x = self.target.marker_data.coord_x
+    local coord_y = self.target.marker_data.coord_y
+    local offset = CalculateOffset(center, coord_x, coord_y)
+    local target_position = center:GetPosition() + offset + Vector3(self.target.marker_data.offset_x, 0, self.target.marker_data.offset_z)
+    self.target.Transform:SetPosition(target_position:Get())
+
     local dist = self.owner:GetDistanceSqToInst(self.target)
     dist = math.sqrt(dist)
 
@@ -45,25 +76,8 @@ function TargetIndicator:OnUpdate(...)
     local scale = Remap(dist, TUNING.MIN_INDICATOR_RANGE, TUNING.MAX_INDICATOR_RANGE, 1, MIN_SCALE)
     self:SetScale(scale)
 
-    -- TODO
     local x, _, z = self.target.Transform:GetWorldPosition()
     self:UpdatePosition(x, z)
-
-    -- local interiorspawner = TheWorld.component.interiorspawner
-    -- if interiorspawner then
-    --     if interiorspawner:GetInteriorCenter(self.owner) then
-
-    --     end
-    -- end
-end
-
-local update_position = TargetIndicator.UpdatePosition
-function TargetIndicator:UpdatePosition(x, z, ...)
-    if self.target.prefab ~= "target_indicator_marker" then
-        return update_position(self, x, z, ...)
-    end
-
-    -- TODO
 end
 
 local get_avatar_atlas = TargetIndicator.GetAvatarAtlas
