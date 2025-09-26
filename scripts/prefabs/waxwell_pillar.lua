2d548e7b5b95673c39668ac4104f57eb1c183f9f
Ziwbi <cd2bfe@gmail.com>
2025-09-26 20:30:29 +0100

    tweak chain range values

--- scripts/prefabs/waxwell_pillar.lua
+++ scripts/prefabs/waxwell_pillar.lua
@@ -6,15 +6,22 @@ local prefabs = {
     "statue_transition_2",
 }
 
-local START_CHAIN_RANGE = 4
-local STOP_CHAIN_RANGE = 8
+-- 现在的数值是寻找3地皮内的生物 
+-- 生物走到3地皮外时 拽到2地皮内
+-- 走到4地皮外就扯断锁链
+
+local FIND_CHAIN_RANGE = 3 * TILE_SCALE
+local KEEP_CHAIN_RANGE = 2 * TILE_SCALE
+local STOP_CHAIN_RANGE = 4 * TILE_SCALE
 
 local BreakChain, DoChainTask
 
 BreakChain = function(inst)
-
     if inst._pull_task then
         inst._pull_task:Cancel()
+    end
+    if inst._damage_task then
+        inst._damage_task:Cancel()
     end
 
     if inst._target and inst._target:IsValid() and inst._target.components.physicsmodifiedexternally then
@@ -31,7 +38,6 @@ BreakChain = function(inst)
     end
 end
 
--- 限制生物的接口
 local function DoPullTask(inst)
     local target = inst._target
     if not target or not target:IsValid() then
@@ -42,21 +48,20 @@ local function DoPullTask(inst)
     local tx, ty, tz = target.Transform:GetWorldPosition()
 
     local distsq = (x-tx)^2 + (z-tz)^2
-    if distsq < STOP_CHAIN_RANGE * STOP_CHAIN_RANGE then -- 检测是否在范围内
+    if distsq < KEEP_CHAIN_RANGE * KEEP_CHAIN_RANGE then -- 2地皮内停止拖拽
+        target.components.physicsmodifiedexternally:SetVelocityForSource(inst, 0, 0)
         return
     end
 
-    -- 一定范围外才拉进来
-    -- 或许该写个一定范围外扯断
-    if distsq >= START_CHAIN_RANGE * START_CHAIN_RANGE then
+    if distsq >= FIND_CHAIN_RANGE * FIND_CHAIN_RANGE then
         local scale = (target:HasTag("smallcreature") and 3) or (target:HasTag("largecreature") and 1) or 2 -- 这里用的标签检测生物大小
-        local angle = inst:GetAngleToPoint(tx, 0, tz)
-        local velx, velz = (scale) * math.cos(angle * DEGREES), (scale) * math.sin(angle * DEGREES)
+        local angle = target:GetAngleToPoint(x, 0, z)
+        local velx, velz = (scale) * math.cos(angle * DEGREES), (-scale) * math.sin(angle * DEGREES)
 
+        -- maybe use easing function here to smooth things up a bit
         target.components.physicsmodifiedexternally:SetVelocityForSource(inst, velx, velz) -- 施加一个朝向柱子的速度向量
-        target.components.combat:GetAttacked(inst, 10) -- 拉仇恨
-    else
-        target.components.physicsmodifiedexternally:SetVelocityForSource(inst, 0, 0)
+    elseif distsq >= STOP_CHAIN_RANGE * STOP_CHAIN_RANGE then
+        BreakChain(inst) -- 8地皮外扯断
     end
 end
 
@@ -64,7 +69,7 @@ local CHAIN_MUST_TAGS = {"_combat"}
 local CHAIN_ONEOF_TAGS = {"character", "animal", "monster", "insect", "smallcreature", "largecreature"}
 local CHAIN_NO_TAGS = {"INLIMBO", "player", "bird"}
 DoChainTask = function(inst)
-    local ent = FindClosestEntity(inst, START_CHAIN_RANGE, true, CHAIN_MUST_TAGS, CHAIN_NO_TAGS, CHAIN_ONEOF_TAGS)
+    local ent = FindClosestEntity(inst, FIND_CHAIN_RANGE, true, CHAIN_MUST_TAGS, CHAIN_NO_TAGS, CHAIN_ONEOF_TAGS)
     if not ent then
         return
     end
@@ -86,7 +91,13 @@ DoChainTask = function(inst)
         inst._target:AddComponent("physicsmodifiedexternally")
     end
     inst._target.components.physicsmodifiedexternally:AddSource(inst)
-    inst._pull_task = inst:DoPeriodicTask(1, DoPullTask)
+    inst._pull_task = inst:DoPeriodicTask(0.1, DoPullTask)
+    inst._damage_task = inst:DoPeriodicTask(1, function()
+        if not inst._target or inst:GetDistanceSqToInst(inst._target) < FIND_CHAIN_RANGE * FIND_CHAIN_RANGE then
+            return
+        end
+        inst._target.components.combat:GetAttacked(inst, 10)
+    end) -- 拉仇恨
     inst:ListenForEvent("remove", BreakChain, inst._target)
     inst:ListenForEvent("death", BreakChain, inst._target)
     inst:ListenForEvent("death", BreakChain)
@@ -129,7 +140,8 @@ local function fn()
         inst.AnimState:PlayAnimation("pillar_fall")
     end)
 
-    c_showradius({START_CHAIN_RANGE * TILE_SCALE, STOP_CHAIN_RANGE * TILE_SCALE}, inst)
+    -- debug
+    c_showradius({FIND_CHAIN_RANGE * TILE_SCALE, KEEP_CHAIN_RANGE * TILE_SCALE, STOP_CHAIN_RANGE * TILE_SCALE}, inst)
 
     return inst
 end
