11bdf1a7cdb594706b84515329362f07e4dced40
Ziwbi <cd2bfe@gmail.com>
2025-09-26 15:44:08 +0100

    use physicsmodifiedexternally component

--- scripts/prefabs/waxwell_pillar.lua
+++ scripts/prefabs/waxwell_pillar.lua
@@ -9,12 +9,16 @@ local prefabs = {
 local START_CHAIN_RANGE = 4
 local STOP_CHAIN_RANGE = 8
 
-local BreakChain, CheckProximity
+local BreakChain, DoChainTask
 
 BreakChain = function(inst)
 
     if inst._pull_task then
         inst._pull_task:Cancel()
+    end
+
+    if inst._target and inst._target:IsValid() and inst._target.components.physicsmodifiedexternally then
+        inst._target.components.physicsmodifiedexternally:RemoveSource(inst)
     end
 
     inst:RemoveEventCallback("death", BreakChain)
@@ -23,12 +27,12 @@ BreakChain = function(inst)
     inst._target = nil
 
     if not inst.components.health:IsDead() then
-        inst._chain_task = inst:DoPeriodicTask(0.1, CheckProximity)
+        inst._chain_task = inst:DoPeriodicTask(0.1, DoChainTask)
     end
 end
 
 -- 限制生物的接口
-local function PullBack(inst)
+local function DoPullTask(inst)
     local target = inst._target
     if not target or not target:IsValid() then
         return
@@ -42,18 +46,24 @@ local function PullBack(inst)
         return
     end
 
-    local scale = (target:HasTag("smallcreature") and 3) or (target:HasTag("largecreature") and 1) or 2 -- 这里用的标签检测生物大小
-    local angle = inst:GetAngleToPoint(tx, 0, tz)
-    local new_x, new_z = tx - scale * math.sin(angle), tz - scale * math.cos(angle)
+    -- 一定范围外才拉进来
+    -- 或许该写个一定范围外扯断
+    if distsq >= START_CHAIN_RANGE * START_CHAIN_RANGE then
+        local scale = (target:HasTag("smallcreature") and 3) or (target:HasTag("largecreature") and 1) or 2 -- 这里用的标签检测生物大小
+        local angle = inst:GetAngleToPoint(tx, 0, tz)
+        local velx, velz = (scale) * math.cos(angle * DEGREES), (scale) * math.sin(angle * DEGREES)
 
-    target.Transform:SetPosition(new_x, ty, new_z) -- SetPosition暴力限制行动，，，
-    target.components.combat:GetAttacked(inst, 10) -- 拉仇恨
+        target.components.physicsmodifiedexternally:SetVelocityForSource(inst, velx, velz) -- 施加一个朝向柱子的速度向量
+        target.components.combat:GetAttacked(inst, 10) -- 拉仇恨
+    else
+        target.components.physicsmodifiedexternally:SetVelocityForSource(inst, 0, 0)
+    end
 end
 
 local CHAIN_MUST_TAGS = {"_combat"}
 local CHAIN_ONEOF_TAGS = {"character", "animal", "monster", "insect", "smallcreature", "largecreature"}
-local CHAIN_NO_TAGS = {"INLIMBO", "player"}
-CheckProximity = function(inst)
+local CHAIN_NO_TAGS = {"INLIMBO", "player", "bird"}
+DoChainTask = function(inst)
     local ent = FindClosestEntity(inst, START_CHAIN_RANGE, true, CHAIN_MUST_TAGS, CHAIN_NO_TAGS, CHAIN_ONEOF_TAGS)
     if not ent then
         return
@@ -62,7 +72,7 @@ CheckProximity = function(inst)
     inst._target = ent
 
     local x, y, z = inst._target.Transform:GetWorldPosition()
-    local fx = SpawnPrefab("statue_transition_2")
+    local fx = SpawnPrefab("statue_transition_2") -- placeholder fx for chain
     if fx ~= nil then
         fx.Transform:SetPosition(x, y, z)
         fx.Transform:SetScale(1, 2, 1)
@@ -72,7 +82,11 @@ CheckProximity = function(inst)
         inst._chain_task:Cancel()
     end
 
-    inst._pull_task = inst:DoPeriodicTask(1, PullBack)
+    if not inst._target.components.physicsmodifiedexternally then
+        inst._target:AddComponent("physicsmodifiedexternally")
+    end
+    inst._target.components.physicsmodifiedexternally:AddSource(inst)
+    inst._pull_task = inst:DoPeriodicTask(1, DoPullTask)
     inst:ListenForEvent("remove", BreakChain, inst._target)
     inst:ListenForEvent("death", BreakChain, inst._target)
     inst:ListenForEvent("death", BreakChain)
@@ -109,11 +123,13 @@ local function fn()
 
     inst.persists = false
 
-    inst._chain_task = inst:DoPeriodicTask(0.1, CheckProximity)
+    inst._chain_task = inst:DoPeriodicTask(0.1, DoChainTask)
 
     inst:ListenForEvent("death", function()
         inst.AnimState:PlayAnimation("pillar_fall")
     end)
+
+    c_showradius({START_CHAIN_RANGE * TILE_SCALE, STOP_CHAIN_RANGE * TILE_SCALE}, inst)
 
     return inst
 end
