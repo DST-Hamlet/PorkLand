3950178871720fa78f3531eda137f1220a135f01
Ardent <1260405837@qq.com>
2025-10-20 16:29:35 +0800

    移除了RestartBrain的额外检查，因为游戏本体已修复此漏洞

--- postinit/entityscript.lua
+++ postinit/entityscript.lua
@@ -222,33 +222,6 @@ function EntityScript:GetIsWet(...) -- 危险的写法
     return ret or self:HasTag("temporary_wet")
 end
 
-function EntityScript:GetShouldBrainStopped()
-    local stopped = false
-    if self.components.freezable and self.components.freezable:IsFrozen() then
-        stopped = true
-    end
-    if self.components.sleeper and self.components.sleeper:IsAsleep() then
-        stopped = true
-    end
-    return stopped
-end
-
-local _RestartBrain = EntityScript.RestartBrain
-function EntityScript:RestartBrain(...)
-    if self:GetShouldBrainStopped() then
-        self:StopBrain()
-        if self.brainfn ~= nil then
-            self.brain = self.brainfn()
-            if self.brain ~= nil then
-                self.brain.inst = self
-                self.brain:Stop()
-            end
-        end
-        return
-    end
-    return _RestartBrain(self, ...)
-end
-
 local _GetAdjectivedName = EntityScript.GetAdjectivedName
 function EntityScript:GetAdjectivedName(...)
     local name = self:GetBasicDisplayName()
