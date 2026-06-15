f5e2db2312b61579a3be9f93ba007bce9f466241
Sydney <manutsawee.louis@outlook.com>
2026-06-15 20:33:58 +0800

    fix: issue of Porkland presets not working

--- postinit/shardindex.lua
+++ postinit/shardindex.lua
@@ -1,68 +1,126 @@
 local modname = modname
 local GetModConfigData = GetModConfigData
+local Levels = require("map/levels")
 GLOBAL.setfenv(1, GLOBAL)
 
--- see shardindex.lua line 528
-function ShardIndex:GeneratePorklandWorldGenOverride(slot, shard)
-    local filename = "../worldgenoverride.lua"
-    local override_enabled = GetModConfigData("enable_porkland_preset")
+local LEVELDATA_FILENAME = "../leveldataoverride.lua"
+local WORLDGEN_FILENAME = "../worldgenoverride.lua"
+local PORKLAND_PRESET = "PORKLAND_DEFAULT"
 
-    local function SetPersistentString(data, cb)
-        if shard ~= nil then
-            TheSim:SetPersistentStringInClusterSlot(slot, shard, filename, data, false, cb)
-        else
-            TheSim:SetPersistentString(filename, data, false, cb)
+local function GetPersistentStringForShard(slot, shard, filename, cb)
+    if shard ~= nil then
+        TheSim:GetPersistentStringInClusterSlot(slot, shard, filename, cb)
+    else
+        TheSim:GetPersistentString(filename, cb)
+    end
+end
+
+local function SetPersistentStringForShard(slot, shard, filename, data, cb)
+    if shard ~= nil then
+        TheSim:SetPersistentStringInClusterSlot(slot, shard, filename, data, false, cb)
+    else
+        TheSim:SetPersistentString(filename, data, false, cb)
+    end
+end
+
+local function HasCustomPorklandLevelSettings(slot, shard, cb)
+    GetPersistentStringForShard(slot, shard, LEVELDATA_FILENAME, function(load_success, str)
+        if not (load_success and str and #str > 0) then
+            cb(false)
+            return
         end
+
+        local success, leveldata = RunInSandboxSafe(str)
+        if not (success and leveldata and leveldata.location == "porkland" and type(leveldata.overrides) == "table") then
+            cb(false)
+            return
+        end
+
+        local base_level = Levels.GetDataForSettingsID(leveldata.settings_id or leveldata.id)
+            or Levels.GetDataForWorldGenID(leveldata.worldgen_id or leveldata.id)
+            or Levels.GetDataForLocation(leveldata.location)
+
+        local base_overrides = base_level and base_level.overrides or nil
+        for name, value in pairs(leveldata.overrides) do
+            local default_value = base_overrides and base_overrides[name] or nil
+            if value ~= nil and value ~= "default" and value ~= default_value then
+                cb(true)
+                return
+            end
+        end
+
+        cb(false)
+    end)
+end
+
+-- see shardindex.lua line 528
+function ShardIndex:GeneratePorklandWorldGenOverride(slot, shard, has_custom_settings, cb)
+    local override_enabled = GetModConfigData("enable_porkland_preset")
+    if has_custom_settings then
+        print("Skip forcing Porkland preset because custom Porkland level settings already exist")
+        if cb ~= nil then
+            cb()
+        end
+        return
     end
 
     local onload = function(load_success, str)
         if load_success then
             -- this override config is rarely used, we still need to check if the Porkland preset is used
-            print("Master Shard already exists " .. filename)
+            print("Master Shard already exists " .. WORLDGEN_FILENAME)
 
             local success, worldgenoverride = RunInSandbox(str)
             if success and worldgenoverride then
                 if not override_enabled then
                     print("Force Porkland Preset is disabled")
                 else
-                    if worldgenoverride.preset ~= "PORKLAND_DEFAULT" then
-                        worldgenoverride.preset = "PORKLAND_DEFAULT"
+                    if worldgenoverride.preset ~= PORKLAND_PRESET then
+                        worldgenoverride.preset = PORKLAND_PRESET
                     end
                     local data = DataDumper(worldgenoverride, nil, false)
                     local onload = function(success, err)
                         if success then
-                            print("Successfully overwrote " .. filename .. " to Master Shard")
+                            print("Successfully overwrote " .. WORLDGEN_FILENAME .. " to Master Shard")
                         else
-                            print("Failed to overwrite " .. filename .. " to Master Shard \n Error: " .. tostring(err))
+                            print("Failed to overwrite " .. WORLDGEN_FILENAME .. " to Master Shard \n Error: " .. tostring(err))
+                        end
+                        if cb ~= nil then
+                            cb()
                         end
                     end
 
-                    SetPersistentString(data, onload)
+                    SetPersistentStringForShard(slot, shard, WORLDGEN_FILENAME, data, onload)
+                end
+            else
+                if cb ~= nil then
+                    cb()
                 end
             end
         else
             if not override_enabled then
                 print("Force Porkland Preset is disabled")
+                if cb ~= nil then
+                    cb()
+                end
             else
-                local data = "return {override_enabled = true, preset = \"PORKLAND_DEFAULT\",}"
+                local data = "return {override_enabled = true, preset = \"" .. PORKLAND_PRESET .. "\",}"
                 local onload = function(success, err)
                     if success then
-                        print("Successfully wrote " .. filename .. " to Master Shard")
+                        print("Successfully wrote " .. WORLDGEN_FILENAME .. " to Master Shard")
                     else
-                        print("Failed to write " .. filename .. " to Master Shard \n Error: " .. tostring(err))
+                        print("Failed to write " .. WORLDGEN_FILENAME .. " to Master Shard \n Error: " .. tostring(err))
+                    end
+                    if cb ~= nil then
+                        cb()
                     end
                 end
 
-                SetPersistentString(data, onload)
+                SetPersistentStringForShard(slot, shard, WORLDGEN_FILENAME, data, onload)
             end
         end
     end
 
-    if shard ~= nil then
-        TheSim:GetPersistentStringInClusterSlot(slot, shard, filename, onload)
-    else
-        TheSim:GetPersistentString(filename, onload)
-    end
+    GetPersistentStringForShard(slot, shard, WORLDGEN_FILENAME, onload)
 end
 
 -- AddSimPostInit(function()
@@ -84,9 +142,13 @@ local _GetLevelDataOverride = function(slot, shard, cb, ...)
     local is_workshop_version = modname:find("workshop-")
     -- only running on master shard.
     if not TheNet:GetIsClient() and not TheShard:IsSecondary() then -- and is_workshop_version
-        ShardGameIndex:GeneratePorklandWorldGenOverride(slot, shard)
-
-        print("Huh? Generate Porkland WorldGen Override done?")
+        HasCustomPorklandLevelSettings(slot, shard, function(has_custom_settings)
+            ShardGameIndex:GeneratePorklandWorldGenOverride(slot, shard, has_custom_settings, function()
+                print("Huh? Generate Porkland WorldGen Override done?")
+                GetLevelDataOverride(slot, shard, cb, ...)
+            end)
+        end)
+        return
     end
     return GetLevelDataOverride(slot, shard, cb, ...)
 end
